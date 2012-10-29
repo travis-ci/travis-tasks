fabf72eab9866e2958d9f05174b6c20e0a035496
Sven Fuchs <me@svenfuchs.com>
2012-10-29 10:15:03 +0100

    debug

--- lib/travis/tasks.rb
+++ lib/travis/tasks.rb
@@ -22,8 +22,6 @@ Travis::Exceptions::Reporter.start
 Travis::Notification.setup
 Travis::Mailer.setup
 
-
-
 # Travis::Memory.new(:tasks).report_periodically if Travis.env == 'production'
 # NewRelic.start if File.exists?('config/newrelic.yml')
 
