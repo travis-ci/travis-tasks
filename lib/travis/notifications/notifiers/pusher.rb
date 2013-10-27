2974f35d3becb8d0ccd66a9a6fc4442d72e2a993
Henrik Hodne <henrik@hodne.io>
2013-10-27 16:02:25 -0500

    pusher: Remove parts that are now done in the worker

--- lib/travis/notifications/notifiers/pusher.rb
+++ lib/travis/notifications/notifiers/pusher.rb
@@ -23,8 +23,6 @@ module Travis
           case client_event
           when 'job:log'
             ["job-#{payload[:id]}"]
-          when /^worker/
-            ['workers']
           else
             ['common']
           end
@@ -37,41 +35,15 @@ module Travis
           end
 
           def trigger(channel, payload)
-            parts(payload).each do |part|
-              # TODO: the second argument in meter can be removed when we're sure that apps
-              #       using this have newest travis-support version
-              Travis::Instrumentation.meter('travis.addons.pusher.task.messages', {})
+            # TODO: the second argument in meter can be removed when we're sure that apps
+            #       using this have newest travis-support version
+            Travis::Instrumentation.meter('travis.addons.pusher.task.messages', {})
 
-              begin
-                Travis.pusher[channel].trigger(client_event, part)
-              rescue ::Pusher::Error => e
-                Travis.logger.error("[notifiers:pusher] Could not send event due to Pusher::Error: #{e.message}, event=#{client_event}, payload: #{part.inspect}")
-                raise
-              end
-            end
-          end
-
-          def parts(payload)
-            if client_event == 'job:log' && payload[:_log].present?
-              # split payload into 9kB chunks, the limit is 10 for entire request
-              # body, 1kB should be enough for headers
-              log = payload[:_log]
-              chunkifier = Chunkifier.new(log, chunk_size, :json => true)
-
-              if chunkifier.length > 1
-                # This should never happen when we update travis-worker to split log parts
-                # bigger than 9kB.
-                Travis.logger.warn("[notifiers:pusher] The log part from worker was bigger than 9kB (#{log.to_json.length}B), payload: #{payload.inspect}")
-              end
-
-              chunkifier.each_with_index.map do |part, i|
-                new_payload = payload.dup.merge(:_log => part)
-                new_payload[:number] = "#{new_payload[:number]}.#{i}" unless i == 0
-                new_payload[:final] = new_payload[:final] && chunkifier.length - 1 == i
-                new_payload
-              end
-            else
-              [payload]
+            begin
+              Travis.pusher[channel].trigger(client_event, payload)
+            rescue ::Pusher::Error => e
+              Travis.logger.error("[notifiers:pusher] Could not send event due to Pusher::Error: #{e.message}, event=#{client_event}, payload: #{payload.inspect}")
+              raise
             end
           end
 
