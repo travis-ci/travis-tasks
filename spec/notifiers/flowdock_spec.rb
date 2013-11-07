ef14cebe61548b4e64412ca907cfcf97c4bff808
Henrik Hodne <henrik@hodne.io>
2013-11-07 11:33:08 -0600

    Use a shared notification payload for specs

--- spec/notifiers/flowdock_spec.rb
+++ spec/notifiers/flowdock_spec.rb
@@ -3,60 +3,7 @@ require "travis/notifications/notifiers/flowdock"
 
 describe Travis::Notifications::Notifiers::Flowdock do
   let(:http) { double("http", post: nil) }
-  let(:payload) do
-    {
-      repository: {
-        id: 1,
-        key: nil,
-        slug: "green-eggs/ham",
-        owner_email: "jane@example.com",
-        owner_avatar_url: "https://avatars.example.com/jane",
-      },
-      request: {
-        token: "foobarbaz",
-        head_commit: "abcdef",
-      },
-      commit: {
-        id: 2,
-        sha: "abcdef",
-        branch: "master",
-        message: "Initial commit",
-        committed_at: "2013-10-27T12:34:56Z",
-        author_name: "Jane Doe",
-        author_email: "jane@example.com",
-        committer_name: "Jane Doe",
-        committer_email: "jane@example.com",
-        committer_email: "jane@example.com",
-        compare_url: "http://travis.example.com/compare/1",
-      },
-      build: {
-        id: 3,
-        repository_id: 1,
-        commit_id: 2,
-        number: "1",
-        pull_request: false,
-        config: {
-          language: "ruby",
-          rvm: "2.0.0",
-          notifications: {
-            flowdock: "abcdef12345",
-          },
-        },
-        state: "passed",
-        previous_state: "failed",
-        started_at: "2013-10-27T12:35:00Z",
-        finished_at: "2013-10-27T12:36:00Z",
-        duration: 60,
-        job_ids: [4],
-      },
-      jobs: [{
-        id: 4,
-        number: "1",
-        state: "passed",
-        tags: [],
-      }],
-    }
-  end
+  let(:payload) { notification_payload }
   let(:params) { { targets: ["abcdef12345"] } }
   subject(:flowdock) { described_class.new(payload, params) }
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,5 @@ RSpec.configure do |config|
   end
 end
 
+require "support/payloads"
 require "travis/notifications"
