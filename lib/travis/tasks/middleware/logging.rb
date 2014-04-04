9856841dcfd535840bca57203e1c3f54e232aa50
Mathias Meyer <meyer@paperplanes.de>
2014-04-04 18:03:56 +0200

    Log time used for a worker task.

--- lib/travis/tasks/middleware/logging.rb
+++ lib/travis/tasks/middleware/logging.rb
@@ -3,6 +3,10 @@ module Travis
     module Middleware
       class Logging
         def call(worker, message, queue, &block)
+          time = Benchmark.ms do
+            yield
+          end
+        ensure
           uuid, _, _, payload, params = *message['args']
           data = Hash.new.tap do |data|
             data['type'] = queue
@@ -19,9 +23,9 @@ module Travis
             data['event'] = params['event'] if params['event']
             data['uuid'] = uuid
             data['job'] = payload['id'] if params['event'] && params['event'] =~ /^job/
+            data['time'] = "%.3f" % (time/1000)
           end
           log(data)
-          yield
         end
 
         def log(data)
