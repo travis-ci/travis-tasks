c38b8571f33fb974a723cf6eeed5133c96810230
Sven Fuchs <me@svenfuchs.com>
2014-10-12 16:39:07 +0200

    use Travis::Metrics.setup

--- lib/travis/tasks.rb
+++ lib/travis/tasks.rb
@@ -58,10 +58,5 @@ if Travis.config.sentry
   Travis::Exceptions::Reporter.start
 end
 
-if Travis.config.librato
-  email, token, source = Travis.config.librato.email, Travis.config.librato.token, Travis.config.librato_source
-  $metriks_reporter = Metriks::LibratoMetricsReporter.new(email, token, source: source)
-  $metriks_reporter.start
-end
-
+Travis::Metrics.setup
 Travis::Addons::Email.setup
