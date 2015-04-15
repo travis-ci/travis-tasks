3bd1e0126fb4262d9f07a1c3d258dc5611adb258
Hiro Asari <asari.ruby@gmail.com>
2015-04-15 12:45:10 -0400

    Log email recipients if delivery is successful
    Addresses are truncated if components are longer than 3 characters
    
    There is no spec here; I do not think the email address format should
    be specified.
    
    Squashed-By: Henrik Hodne <henrik@travis-ci.com>
    
    Closes #33

--- lib/travis/addons/email/task.rb
+++ lib/travis/addons/email/task.rb
@@ -21,7 +21,10 @@ module Travis
         private
 
           def process
-            Mailer::Build.finished_email(payload, recipients, broadcasts).deliver if recipients.any?
+            if recipients.any?
+              Mailer::Build.finished_email(payload, recipients, broadcasts).deliver
+              info "status=sent msg='email sent' #{recipients.map { |r| 'email=' + obfuscate_email_address(r) }.join(' ')}"
+            end
           rescue Net::SMTPServerBusy => e
             error("Could not send email to: #{recipients} (error: #{e.message})")
             raise unless e.message =~ /Bad recipient address syntax/ || e.message =~ /Recipient address rejected/
@@ -39,6 +42,19 @@ module Travis
             mail.domain && mail.address == email && (tree.domain.dot_atom_text.elements.size > 1)
           rescue Exception => e
             false
+          end
+
+          def obfuscate_email_address(add)
+            match_data = add.match /^(?<name>[^@]+)@(?<domain>.+?)\.(?<tld>[^\.]+)$/
+            return add unless match_data
+
+            name       = match_data[:name]
+            domain     = match_data[:domain]
+            tld        = match_data[:tld]
+            name       = name[0,3]   + '...' if name.length > 3
+            domain     = domain[0,3] + '...' if domain.length > 3
+
+            name + '@' + domain + '.' + tld
           end
       end
     end
