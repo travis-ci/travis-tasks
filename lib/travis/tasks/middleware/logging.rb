f7568ba9882ca5b05abdbbf41d4b7c29aa8b8796
Hiro Asari <asari.ruby@gmail.com>
2015-11-25 11:04:58 -0500

    Use payload['pull_request'] to set data['event']

--- lib/travis/tasks/middleware/logging.rb
+++ lib/travis/tasks/middleware/logging.rb
@@ -20,17 +20,16 @@ module Travis
               data['repo'] = payload['repository']['slug']
             end
 
-            data['event'] = params['event'] if params['event']
             data['uuid'] = uuid
             data['job'] = payload['id'] if params['event'] && params['event'] =~ /^job/
             data['time'] = "%.3f" % (time/1000) if time
             data['jid'] = message['jid']
 
             if payload['pull_request']
-              data['pull_request'] = 'yes'
+              data['event'] = 'pull_request'
               data['pull_request_number'] = payload['pull_request_number']
             else
-              data['pull_request'] = 'no'
+              data['event'] = 'push'
             end
           end
           log(data)
