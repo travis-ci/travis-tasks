42947c9f645d4afebb594eac9d3bc070b81d5598
Curtis Ekstrom <code@fauxton.io>
2016-08-23 15:53:55 +0200

    Use double-quotes

--- spec/addons/webhook/task_spec.rb
+++ spec/addons/webhook/task_spec.rb
@@ -1,5 +1,5 @@
-require 'spec_helper'
-require 'rack'
+require "spec_helper"
+require "rack"
 
 describe Travis::Addons::Webhook::Task do
   include Travis::Testing::Stubs
@@ -8,7 +8,7 @@ describe Travis::Addons::Webhook::Task do
   let(:http)    { Faraday::Adapter::Test::Stubs.new }
   let(:client)  { Faraday.new { |f| f.request :url_encoded; f.adapter :test, http } }
   let(:payload) { Marshal.load(Marshal.dump(WEBHOOK_PAYLOAD)) }
-  let(:repo_slug) { 'svenfuchs/minimal' }
+  let(:repo_slug) { "svenfuchs/minimal" }
 
   before do
     Travis.config.notifications = [:webhook]
@@ -17,17 +17,17 @@ describe Travis::Addons::Webhook::Task do
   end
 
   def run(targets)
-    subject.new(payload, targets: targets, token: '123456').run
+    subject.new(payload, targets: targets, token: "123456").run
   end
 
-  it 'posts to the given targets, with the given payload and the given access token' do
-    targets = ['http://one.webhook.com/path', 'http://second.webhook.com/path']
+  it "posts to the given targets, with the given payload and the given access token" do
+    targets = ["http://one.webhook.com/path", "http://second.webhook.com/path"]
 
     targets.each do |url|
       uri = URI.parse(url)
       http.post uri.path do |env|
         env[:url].host.should == uri.host
-        env[:request_headers]['Authorization'].should == authorization_for(repo_slug, '123456')
+        env[:request_headers]["Authorization"].should == authorization_for(repo_slug, "123456")
         payload_from(env).keys.sort.should == payload.keys.map(&:to_s).sort
       end
     end
@@ -55,9 +55,9 @@ describe Travis::Addons::Webhook::Task do
     uri = URI.parse(url)
     http.post uri.path do |env|
       env[:url].host.should == uri.host
-      auth = env[:request_headers]['Authorization']
-      auth.should == 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
-      auth.should == Faraday::Request::BasicAuthentication.header('Aladdin', 'open sesame')
+      auth = env[:request_headers]["Authorization"]
+      auth.should == "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="
+      auth.should == Faraday::Request::BasicAuthentication.header("Aladdin", "open sesame")
       payload_from(env).keys.sort.should == payload.keys.map(&:to_s).sort
     end
 
@@ -65,12 +65,12 @@ describe Travis::Addons::Webhook::Task do
     http.verify_stubbed_calls
   end
 
-  it 'includes a Travis-Repo-Slug header' do
-    url = 'https://one.webhook.com/path'
+  it "includes a Travis-Repo-Slug header" do
+    url = "https://one.webhook.com/path"
     uri = URI.parse(url)
     http.post uri.path do |env|
       env[:url].host.should == uri.host
-      env[:request_headers]['Travis-Repo-Slug'].should == repo_slug
+      env[:request_headers]["Travis-Repo-Slug"].should == repo_slug
       payload_from(env).keys.sort.should == payload.keys.map(&:to_s).sort
     end
 
@@ -79,7 +79,7 @@ describe Travis::Addons::Webhook::Task do
   end
 
   def payload_from(env)
-    JSON.parse(Rack::Utils.parse_query(env[:body])['payload'])
+    JSON.parse(Rack::Utils.parse_query(env[:body])["payload"])
   end
 
   def authorization_for(slug, token)
