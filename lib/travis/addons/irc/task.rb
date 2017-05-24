4a8c868ae863448f4170dd704a9455e1305d3ad4
Igor Wiedler <igor@travis-ci.org>
2017-05-24 21:28:07 +0200

    irc: remove non-existent method call

--- lib/travis/addons/irc/task.rb
+++ lib/travis/addons/irc/task.rb
@@ -113,7 +113,7 @@ module Travis
           end
 
           def freenode?(host)
-            (host.end_with?('.freenode.net') || host.end_with?('.freenode.com')) && nick == 'travis-ci' && freenode_dns?(host)
+            (host.end_with?('.freenode.net') || host.end_with?('.freenode.com')) && nick == 'travis-ci'
           end
 
           def try_config(option)
