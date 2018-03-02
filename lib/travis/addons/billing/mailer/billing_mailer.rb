0633f23a142e8cc6bcf08e38748eebad91d63b2e
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-03-02 15:52:39 +0100

    Modify emails so they get send

--- lib/travis/addons/billing/mailer/billing_mailer.rb
+++ lib/travis/addons/billing/mailer/billing_mailer.rb
@@ -12,6 +12,7 @@ module Travis
           def charge_failed(subscription, owner, charge, event)
             @subscription, @owner, @charge, @event = subscription, owner, charge, event
             @billing_email = @subscription[:billing_email]
+            @next_payment_attempt = Time.at(@event[:next_payment_attempt]).strftime('%F')
             subject = "Travis CI: Charging Your Credit Card Failed"
             mail(from: from, to: @billing_email, subject: subject, template_path: 'billing_mailer')
           end
