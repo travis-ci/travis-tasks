3d9e9f24352c687dcd029d34cd11f394bc77a970
Hiro Asari <asari.ruby@gmail.com>
2018-04-22 22:07:33 -0400

    Rework GithubCheckStatus spec
    To simply check for GitHub API calls

--- spec/addons/github_check_status/task_spec.rb
+++ spec/addons/github_check_status/task_spec.rb
@@ -6,25 +6,28 @@ describe Travis::Addons::GithubCheckStatus::Task do
   before { ENV['GITHUB_PRIVATE_PEM'] = File.read('spec/fixtures/github_pem.txt') }
   after  { ENV.delete('GITHUB_PRIVATE_PEM') }
 
-  let(:subject)    { Travis::Addons::GithubCheckStatus::Task }
-  let(:url)        { '/repos/svenfuchs/minimal/statuses/62aae5f70ceee39123ef' }
-  let(:target_url) { 'https://travis-ci.org/svenfuchs/minimal/builds/1?utm_source=github_status&utm_medium=notification' }
+  let(:subject)    { Travis::Addons::GithubCheckStatus::Task.new(payload, installation: installation_id) }
   let(:payload)    { Marshal.load(Marshal.dump(TASK_PAYLOAD)) }
   let(:io)         { StringIO.new }
-  let(:gh_apps)    { Travis::GithubApps.new(accept_header: { "Accept" => "application/vnd.github.antiope-preview+json" }) }
+  let(:gh_apps)    { Travis::GithubApps.new installation_id }
+  let(:installation_id) { '12345' }
+
+  let(:conn) {
+    Faraday.new do |builder|
+      builder.adapter :test do |stub|
+        stub.post("/installations/12345/access_tokens") { |env| [201, {}, "{\"token\":\"github_apps_access_token\",\"expires_at\":\"2018-04-03T20:52:14Z\"}"] }
+        stub.post("/repos/svenfuchs/minimal/check-runs") { |env| [201, {}, "{}"] }
+      end
+    end
+  }
 
   before do
     Travis.logger = Logger.new(io)
   end
 
-   def run
-    subject.new(payload, tokens: { 'svenfuchs' => '12345', 'jdoe' => '67890' }).run
+  it 'makes expected API calls' do
+    subject.expects(:github_apps).returns(gh_apps)
+    gh_apps.expects(:github_api_conn).with().returns(conn)
+    subject.run
   end
-
-  it 'authenticates using the token passed into the task' do
-    gh_apps.expects(:with).with { |options| options[:token] == '12345' }.returns({})
-    run
-  end
-
-
 end
