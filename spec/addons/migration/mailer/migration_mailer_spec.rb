d85384b2becf68652376a85ad4d550d15757bedb
Pavel Dotsulenko <pavel@assembla.com>
2019-05-28 11:41:46 +0300

    Add beta confirmation mailer
    * Fix spec

--- spec/addons/migration/mailer/migration_mailer_spec.rb
+++ spec/addons/migration/mailer/migration_mailer_spec.rb
@@ -12,7 +12,7 @@ describe Travis::Addons::Migration::Mailer::MigrationMailer do
       expect(mail.to).to eq(recipients)
       expect(mail.from).to eq(['success@travis-ci.com'])
       expect(mail.subject).to eq("Your account, @#{user_name}, is ready to start migrating!")
-      expect(mail.body).to match("Welcome to the beta!")
+      expect(mail.body).to match('Welcome to the migration beta!')
     end
   end
 end
