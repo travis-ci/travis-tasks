a27c71e90fe94022e5b08477859d82d93b37c1c7
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-06-06 15:10:23 +0200

    Add logginf output

--- lib/travis/addons/billing/task.rb
+++ lib/travis/addons/billing/task.rb
@@ -44,7 +44,7 @@ module Travis
           def send_email
             Mailer::BillingMailer.public_send(params[:email_type], recipients, subscription, owner, charge, event, invoice, cc_last_digits).deliver
             emails = recipients.map { |r| 'email=' + obfuscate_email_address(r) }.join(' ')
-            info "type=#{type} status=sent msg='email sent #{emails}"
+            info "type=#{type} status=sent msg='email sent #{emails} for #{params[:email_type]}"
           end
       end
     end
