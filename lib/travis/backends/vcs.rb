907690d8904a6c317520d947c7701a2ca7d4ee65
Rafael <rafael@travis-ci.org>
2019-12-06 20:00:17 -0300

    Fix method signatures

--- lib/travis/backends/vcs.rb
+++ lib/travis/backends/vcs.rb
@@ -21,7 +21,7 @@ module Travis
         client.get("/repos/#{id}/checks", vcs_type: type, commit: ref, check_run_name: check_run_name)
       end
 
-      def create_status(id:, type:, ref:, payload:)
+      def create_status(process_via_gh_apps:, id:, type:, ref:, payload:)
         client.post("/repos/#{id}/status", vcs_type: type, commit: ref, payload: payload)
       end
 
