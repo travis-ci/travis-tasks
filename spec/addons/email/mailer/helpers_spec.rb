b46e5a40c6d9a0bacfb490c31ab94b7d785ee05e
Tahsin Hasan <tahsin@travis-ci.org>
2020-01-08 21:56:26 +0600

    remove unsubscribe

--- spec/addons/email/mailer/helpers_spec.rb
+++ spec/addons/email/mailer/helpers_spec.rb
@@ -56,12 +56,4 @@ describe Travis::Addons::Email::Mailer::Helpers do
   it '#repository_build_url returns correct URL' do
     expect(repository_build_url(slug: 'svenfuchs/minimal', id: 111)).to match_url 'https://travis-ci.org/svenfuchs/minimal/builds/111?utm_source=email&utm_medium=notification'
   end
-
-  it '#unsubscribe_url returns correct URL' do
-    expect(unsubscribe_url).to match_url 'https://travis-ci.org/account/preferences/unsubscribe?utm_source=email&utm_medium=notification'
-  end
-
-  it '#repository_unsubscribe_url returns correct URL' do
-    expect(repository_unsubscribe_url(repository)).to match_url "https://travis-ci.org/account/preferences/unsubscribe?utm_source=email&utm_medium=notification&repository=#{repository.id}"
-  end
 end
