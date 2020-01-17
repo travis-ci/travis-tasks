703021c0701019b1c20f82b6a09b8f0f720b66af
Piotr Milcarz <piotrm@travis-ci.org>
2020-01-17 12:13:38 +0100

    debug

--- lib/travis/api.rb
+++ lib/travis/api.rb
@@ -8,10 +8,8 @@ module Travis
 
     def backend(vcs_id, vcs_type, installation_id: nil)
       if Travis::Rollout.matches?(:vcs, id: vcs_id) || !vcs_type.match(/Github/)
-        Travis.logger.info('API.backend: Travis::Backends::Vcs.new')
         Travis::Backends::Vcs.new
       else
-        Travis.logger.info('API.backend: Travis::Backends::Github.new(installation_id)')
         Travis::Backends::Github.new(installation_id)
       end
     end
