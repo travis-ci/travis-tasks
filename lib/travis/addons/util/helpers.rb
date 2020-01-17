594d87206ec2bd673c1909f633a1feb026a6893a
Piotr Milcarz <piotrm@travis-ci.org>
2020-01-17 11:37:43 +0100

    fix

--- lib/travis/addons/util/helpers.rb
+++ lib/travis/addons/util/helpers.rb
@@ -4,6 +4,7 @@ module Travis
   module Addons
     module Util
       module Helpers
+        extend self
 
         def vcs_prefix(vcs_type)
           vcs_type.sub('Repository', '').downcase if vcs_type
