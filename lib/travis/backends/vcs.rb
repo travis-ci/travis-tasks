271f5182a92f5e215e8a51292568ec4ffba8d37f
Piotr Milcarz <piotrm@travis-ci.org>
2020-01-17 14:50:55 +0100

    CGI::escape

--- lib/travis/backends/vcs.rb
+++ lib/travis/backends/vcs.rb
@@ -9,38 +9,38 @@ module Travis
       end
 
       def create_check_run(id:, type:, payload:)
-        client.post("/repos/#{id}/checks", vcs_type: type, payload: payload)
+        client.post("/repos/#{CGI::escape(id)}/checks", vcs_type: type, payload: payload)
       end
 
       def update_check_run(id:, type:, check_run_id:, payload:)
-        client.post("/repos/#{id}/checks", vcs_type: type, id: check_run_id, payload: payload)
+        client.post("/repos/#{CGI::escape(id)}/checks", vcs_type: type, id: check_run_id, payload: payload)
       end
 
       def check_runs(id:, type:, ref:, check_run_name:)
-        client.get("/repos/#{id}/checks", vcs_type: type, commit: ref, check_run_name: check_run_name)
+        client.get("/repos/#{CGI::escape(id)}/checks", vcs_type: type, commit: ref, check_run_name: check_run_name)
       end
 
       def create_status(process_via_gh_apps:, id:, type:, ref:, payload:)
-        client.post("/repos/#{id}/status", vcs_type: type, commit: ref, payload: payload)
+        client.post("/repos/#{CGI::escape(id)}/status", vcs_type: type, commit: ref, payload: payload)
       end
 
       def file_url(id:, type:, slug:, branch:, file:)
-        response = client.get("/repos/#{id}/urls/file", vcs_type: type, branch: branch, file: file)
+        response = client.get("/repos/#{CGI::escape(id)}/urls/file", vcs_type: type, branch: branch, file: file)
         JSON.parse(response.body)[:url] if response.success?
       end
 
       def branch_url(id:, type:, slug:, branch:)
         Travis.logger.info("Travis::Backends::Vcs id: " + id.to_s)
-        response = client.get("/repos/#{id}/urls/branch", vcs_type: type, branch: branch)
+        response = client.get("/repos/#{CGI::escape(id)}/urls/branch", vcs_type: type, branch: branch)
         JSON.parse(response.body)[:url] if response.success?
       end
 
       def create_check_run_url(id)
-        "#{Travis.config.vcs.url}/repos/#{id}/checks"
+        "#{Travis.config.vcs.url}/repos/#{CGI::escape(id)}/checks"
       end
 
       def create_status_url(id, _ref)
-        "#{Travis.config.vcs.url}/repos/#{id}/status"
+        "#{Travis.config.vcs.url}/repos/#{CGI::escape(id)}/status"
       end
 
     private
