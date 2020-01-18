2ba80991ddf4cb966d51d30c9cf69b1a788db4b8
Piotr Milcarz <piotrm@travis-ci.org>
2020-01-18 10:23:25 +0100

    fix

--- lib/travis/backends/vcs.rb
+++ lib/travis/backends/vcs.rb
@@ -31,8 +31,7 @@ module Travis
 
       def branch_url(id:, type:, slug:, branch:)
         response = client.get("/repos/#{CGI::escape(id)}/urls/branch", vcs_type: type, branch: branch)
-        Travis.logger.info("Result of /repos/#{CGI::escape(id)}/urls/branch: #{response.success?.to_s} #{response.body.to_s} : #{JSON.parse(response.body)[:url]} bez #{JSON.parse(response.body)['url']}")
-        JSON.parse(response.body)[:url] if response.success?
+        JSON.parse(response.body)['url'] if response.success?
       end
 
       def create_check_run_url(id)
