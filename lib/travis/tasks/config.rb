529a7210289de7a2c55ab5fbb90c283e2bffa295
Artur Kremens <artur@assembla.com>
2020-01-22 13:50:43 +0100

    tmp

--- lib/travis/tasks/config.rb
+++ lib/travis/tasks/config.rb
@@ -44,7 +44,7 @@ module Travis
              librato: { email: nil, token: nil },
              auth:    { jwt_public_key: ENV['JWT_RSA_PUBLIC_KEY'], http_basic_auth: http_basic_auth },
              github_apps: { debug: ENV['GITHUB_APPS_DEBUG'] },
-             vcs: { url: ENV['VCS_URL'], token: ENV['VCS_TOKEN'] }
+             vcs: {}
 
       default _access: [:key]
 
