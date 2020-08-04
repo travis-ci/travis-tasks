ba0fe2e48cf00e04b6bcd2ef6adc19eb91850fa3
Piotr Milcarz <piotr.milcarz@gmail.com>
2020-08-04 17:43:12 +0200

    New plans emails - welcome

---
+++ lib/travis/addons/plan/task.rb
@@ -0,0 +1,31 @@
+require 'travis/addons/email/task'
+require 'travis/addons/trial/mailer/trial_mailer'
+
+module Travis
+  module Addons
+    module Plan
+
+      # Sends out plan email using ActionMailer.
+      class Task < Travis::Addons::Email::Task
+        def type
+          :"#{params[:stage]}"
+        end
+
+        def owner
+          params[:owner]
+        end
+
+        def plan
+          params.fetch(:plan, 'Free Tier Plan').to_s
+        end
+
+        private
+
+          def send_email
+            Mailer::PlanMailer.public_send(params[:stage], recipients, owner, plan).deliver
+            info "type=#{type} status=sent msg='email sent' #{recipients.map { |r| 'email=' + obfuscate_email_address(r) }.join(' ')}"
+          end
+      end
+    end
+  end
+end
