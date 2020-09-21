0295db3a86b9d6a9fb66a8b6d6ef18f8c51afe12
Tahsin Hasan <tahsin@travis-ci.org>
2020-09-21 12:10:53 +0600

    redis connection pool

--- lib/travis/addons/github_status/task.rb
+++ lib/travis/addons/github_status/task.rb
@@ -1,3 +1,4 @@
+require 'gh'
 module Travis
   module Addons
     module GithubStatus
@@ -22,6 +23,15 @@ module Travis
           'error'   => 'The Travis CI build could not complete due to an error',
         }
 
+        ERROR_REASONS = {
+          401 => :incorrect_auth,
+          403 => :incorrect_auth_or_suspended_acct_or_rate_limited,
+          404 => :repo_not_found_or_incorrect_auth,
+          422 => :maximum_number_of_statuses,
+        }
+
+        REDIS_PREFIX = 'travis-tasks:github-status:'.freeze
+
         def url
           "/repos/#{repository[:slug]}/statuses/#{sha}"
         end
@@ -29,15 +39,56 @@ module Travis
         private
 
           def process(timeout)
-            info("type=github_status build=#{build[:id]} repo=#{repository[:slug]} state=#{state} commit=#{sha} tokens_count=#{tokens.size}")
+            users_tried = []
+            status      = :not_ok
+            message = %W[
+              type=github_status
+              build=#{build[:id]}
+              repo=#{repository[:slug]}
+              state=#{state}
+              commit=#{sha}
+              tokens_count=#{tokens.size}
+            ].join(' ')
 
-            tokens.each do |username, token|
-              if process_with_token(username, token)
-                return
-              else
-                error("type=github_status build=#{build[:id]} repo=#{repository[:slug]} error=not_updated commit=#{sha} username=#{username} url=#{GH.api_host + url}")
+            while !tokens.empty? and status != :ok do
+              username, token = tokens.shift
+              unless token
+                error("#{message} username=#{username} token=#{token.to_s}")
+                next
               end
+
+              status, details = process_with_token(username, token)
+              if status == :ok
+                info("#{message} username=#{username} processed_with=user_token token=#{token[0,3]}...")
+                return
+              elsif status == :skipped
+                info "#{message} message=\"Token for #{username} failed within the last hour. Skipping\""
+                next
+              end
+
+              # we can't post any more status to this commit, so there's
+              # no point in trying further
+              return if details[:status].to_i == 422
+
+              users_tried << username
+              error(%W[
+                type=github_status
+                build=#{build[:id]}
+                repo=#{repository[:slug]}
+                error=not_updated
+                commit=#{sha}
+                username=#{username}
+                url=#{url}
+                github_response=#{details[:status]}
+                processed_with=user_token
+                users_tried=#{users_tried}
+                last_token_tried="#{token.to_s[0,3]}..."
+                rate_limit=#{rate_limit_info details[:response_headers]}
+                github_request_id=#{github_request_id details[:response_headers]}
+              ].join(' '))
             end
+
+            error("#{message} message=\"All known tokens failed to update status\"")
           end
 
           def tokens
@@ -45,23 +96,61 @@ module Travis
           end
 
           def process_with_token(username, token)
-            authenticated(token) do
+            Travis.redis_pool.with do |redis|
+              if redis.exists(errored_token_key(token))
+                return [:skipped, {}]
+              end
+            end
+
+            value = authenticated(token) do
               GH.post(url, :state => state, :description => description, :target_url => target_url, :context => context)
             end
-          rescue GH::Error(:response_status => 401) => e
-            error("type=github_status build=#{build[:id]} repo=#{repository[:slug]} state=#{state} commit=#{sha} username=#{username} response_status=401 reason=incorrect_auth body=#{e.info[:response_body]}")
-            nil
-          rescue GH::Error(:response_status => 403) => e
-            error("type=github_status build=#{build[:id]} repo=#{repository[:slug]} state=#{state} commit=#{sha} username=#{username} response_status=403 reason=incorrect_auth_or_suspended_acct body=#{e.info[:response_body]}")
-            nil
-          rescue GH::Error(:response_status => 404) => e
-            error("type=github_status build=#{build[:id]} repo=#{repository[:slug]} state=#{state} commit=#{sha} username=#{username} response_status=404 reason=repo_not_found_or_incorrect_auth body=#{e.info[:response_body]}")
-            nil
-          rescue GH::Error(:response_status => 422)
-            error("type=github_status build=#{build[:id]} repo=#{repository[:slug]} state=#{state} commit=#{sha} username=#{username} response_status=422 reason=maximum_number_of_statuses")
-            nil
+            [:ok, value]
+          rescue GH::Error(:response_status => 401),
+                 GH::Error(:response_status => 403),
+                 GH::Error(:response_status => 404),
+                 GH::Error(:response_status => 422) => e
+            mark_token(username, token) if e.info[:response_status] == 403
+            error(%W[
+              type=github_status
+              build=#{build[:id]}
+              repo=#{repository[:slug]}
+              state=#{state}
+              commit=#{sha}
+              username=#{username}
+              response_status=#{e.info[:response_status]}
+              reason=#{ERROR_REASONS.fetch(Integer(e.info[:response_status]))}
+              processed_with=user_token
+              body=#{e.info[:response_body]}
+              last_token_tried="#{token.to_s[0,3]}..."
+              rate_limit=#{rate_limit_info e.info[:response_headers]}
+              github_request_id=#{github_request_id e.info[:response_headers]}
+            ].join(' '))
+
+            return [
+              :error,
+              {
+                status: e.info[:response_status],
+                reason: e.info[:response_body],
+                response_headers: e.info[:response_headers]
+                }
+              ]
           rescue GH::Error => e
-            message = "type=github_status build=#{build[:id]} repo=#{repository[:slug]} error=not_updated commit=#{sha} url=#{GH.api_host + url} message=#{e.message}"
+            message = %W[
+              type=github_status
+              build=#{build[:id]}
+              repo=#{repository[:slug]}
+              error=not_updated
+              commit=#{sha}
+              url=#{url}
+              response_status=#{e.info[:response_status]}
+              message=#{e.message}
+              processed_with=user_token
+              body=#{e.info[:response_body]}
+              last_token_tried="#{token.to_s[0,3]}..."
+              rate_limit=#{rate_limit_info e.info[:response_headers]}
+              github_request_id=#{github_request_id e.info[:response_headers]}
+            ].join(' ')
             error(message)
             raise message
           end
@@ -88,7 +177,25 @@ module Travis
           end
 
           def authenticated(token, &block)
-            GH.with(http_options(token), &block)
+            GH.with(gh_no_tokencheck_stack.build(http_options(token)), &block)
+          end
+
+          def gh_no_tokencheck_stack
+            return @gh_no_tokencheck_stack if @gh_no_tokencheck_stack
+            @gh_no_tokencheck_stack = ::GH::Stack.new do
+              use ::GH::Instrumentation
+              use ::GH::Parallel
+              use ::GH::Pagination
+              use ::GH::LinkFollower
+              use ::GH::MergeCommit
+              use ::GH::LazyLoader
+              use ::GH::Normalizer
+              use ::GH::CustomLimit
+              use ::GH::Remote
+            end
+
+            @gh_no_tokencheck_stack.options.merge! ::GH::DefaultStack.options
+            @gh_no_tokencheck_stack
           end
 
           def http_options(token)
@@ -99,6 +206,51 @@ module Travis
             {
               "Accept" => "application/vnd.github.v3+json"
             }
+          end
+
+          def github_request_id(headers)
+            if headers.respond_to? :[]
+              headers["x-github-request-id"]
+            end
+          end
+
+          def rate_limit_info(headers)
+            return {error: "headers were nil"} unless headers
+
+            unless rate_limit_headers_complete? headers
+              return {error: "response headers did not contain rate limit information"}
+            end
+
+            {
+              limit: headers["x-ratelimit-limit"].to_i,
+              remaining: headers["x-ratelimit-remaining"].to_i,
+              next_limit_reset_in: headers["x-ratelimit-reset"].to_i - Time.now.to_i
+            }
+          end
+
+          def rate_limit_headers_complete?(headers)
+            !headers.nil? &&
+            !headers["x-ratelimit-limit"    ].to_s.empty? &&
+            !headers["x-ratelimit-remaining"].to_s.empty? &&
+            !headers["x-ratelimit-reset"    ].to_s.empty?
+          end
+
+          def github_request_id(headers)
+            if headers.respond_to? :[]
+              headers["x-github-request-id"]
+            end
+          end
+
+          def errored_token_key(token)
+            token_hash = Digest::SHA256.hexdigest(token)
+            REDIS_PREFIX + "errored_tokens:#{token_hash}"
+          end
+
+          def mark_token(login, token)
+            info "message=\"A request with token belonging to #{login} failed. Will skip using this token for 1 hour.\""
+            Travis.redis_pool.with do |redis|
+              redis.set errored_token_key(token), "", ex: 60*60 # an hour
+            end
           end
       end
     end
--- spec/addons/github_status/task_spec.rb
+++ spec/addons/github_status/task_spec.rb
@@ -1,26 +1,44 @@
 require 'spec_helper'
-
+require 'redis'
 describe Travis::Addons::GithubStatus::Task do
   include Travis::Testing::Stubs
 
   let(:subject)    { Travis::Addons::GithubStatus::Task }
+  let(:params)     { { tokens: { 'svenfuchs' => '12345', 'jdoe' => '67890' } } }
+  let(:tokens)     { params.fetch(:tokens, {}).values }
+  let(:instance)   { subject.new(payload, params) }
   let(:url)        { '/repos/svenfuchs/minimal/statuses/62aae5f70ceee39123ef' }
   let(:target_url) { 'https://travis-ci.org/svenfuchs/minimal/builds/1?utm_source=github_status&utm_medium=notification' }
   let(:payload)    { Marshal.load(Marshal.dump(TASK_PAYLOAD)) }
   let(:io)         { StringIO.new }
+  let(:rate_limit_data) { {"x-ratelimit-limit" => "60", "x-ratelimit-remaining" => "59", "x-ratelimit-reset" => (Time.now.to_i + 2000).to_s} }
+  let(:no_tokencheck_stack) { instance.send :gh_no_tokencheck_stack }
+
+  def redis
+    @redis ||= Redis.new(url: Travis.config.redis.url)
+  end
 
   before do
     Travis.logger = Logger.new(io)
+    tokens.each do |t|
+      Redis.new(url: Travis.config.redis.url).del(Travis::Addons::GithubStatus::Task::REDIS_PREFIX + "errored_tokens:" + token_hash(t))
+    end
   end
 
   def run
-    subject.new(payload, tokens: { 'svenfuchs' => '12345', 'jdoe' => '67890' }).run
+    instance.run
+  end
+
+  def token_hash(token)
+    Digest::SHA256.hexdigest(token)
   end
 
   it 'posts status info for a created build' do
     payload["build"]["state"] = 'created'
     GH.expects(:post).with(url, state: 'pending', description: 'The Travis CI build is in progress', target_url: target_url, context: 'continuous-integration/travis-ci/push').returns({})
     run
+
+    expect(io.string).to include('processed_with=')
   end
 
   it 'posts status info for a passed build' do
@@ -48,63 +66,90 @@ describe Travis::Addons::GithubStatus::Task do
   end
 
   it 'authenticates using the token passed into the task' do
-    GH.expects(:with).with { |options| options[:token] == '12345' }.returns({})
+    GH.expects(:with).with(instance_of(GH::Instrumentation)).returns({})
     run
   end
 
   it 'authenticates using the next token if the first token failed' do
-    GH.expects(:with).with { |options| options[:token] == '12345' }.raises(GH::Error.new(nil, nil, response_status: 401))
-    GH.expects(:with).with { |options| options[:token] == '67890' }.returns({})
+    error = { response_status: 403, response_headers: rate_limit_data }
+    GH.stubs(:post).raises(GH::Error.new('failed', nil, error))
+    instrumentation = GH::Instrumentation.new
+    no_tokencheck_stack.expects(:build).with() { |hash| hash[:token] == '12345'}.returns(instrumentation)
+    no_tokencheck_stack.expects(:build).with() { |hash| hash[:token] == '67890'}.returns(instrumentation)
     run
   end
 
   it 'accepts a single token using the legacy payload' do
-    GH.expects(:with).with { |options| options[:token] == '12345' }.returns({})
+    GH.expects(:with).with(instance_of(GH::Instrumentation)).returns({})
     subject.new(payload, token: '12345').run
   end
 
   it 'does not raise if a 422 error was returned by GH' do
-    error = { response_status: 422 }
+    error = { response_status: 422, response_headers: rate_limit_data }
     GH.stubs(:post).raises(GH::Error.new('failed', nil, error))
     expect {
       run
     }.not_to raise_error
-    io.string.should include('response_status=422')
-    io.string.should include('reason=maximum_number_of_statuses')
+    expect(io.string).to include('response_status=422')
+    expect(io.string).to include('reason=maximum_number_of_statuses')
   end
 
-  it 'does not raise if a 403 error was returned by GH' do
-    error = { response_status: 403 }
+  it 'does not raise if a 403 error was returned by GH and marks the token invalid' do
+    error = { response_status: 403, response_headers: rate_limit_data }
     GH.stubs(:post).raises(GH::Error.new('failed', nil, error))
     expect {
       run
     }.not_to raise_error
-    io.string.should include('response_status=403')
-    io.string.should include('reason=incorrect_auth_or_suspended_acct')
+    expect(io.string).to match /A request with token belonging to svenfuchs failed\./
+    expect(io.string).to include('response_status=403')
+    expect(io.string).to include('reason=incorrect_auth_or_suspended_acct')
+    expect(redis.exists(Travis::Addons::GithubStatus::Task::REDIS_PREFIX + 'errored_tokens:' + token_hash('12345'))).to be true
   end
+
 
   it 'does not raise if a 404 error was returned by GH' do
-    error = { response_status: 404 }
+    error = { response_status: 404, response_headers: rate_limit_data }
     GH.stubs(:post).raises(GH::Error.new('failed', nil, error))
     expect {
       run
     }.not_to raise_error
-    io.string.should include('response_status=404')
-    io.string.should include('reason=repo_not_found_or_incorrect_auth')
+    expect(io.string).to include('response_status=404')
+    expect(io.string).to include('reason=repo_not_found_or_incorrect_auth')
+    expect(io.string).to include('users_tried=')
+  end
+
+  context "a user token has been invalidated" do
+    before do
+      tokens.each do |token|
+        redis.set(Travis::Addons::GithubStatus::Task::REDIS_PREFIX + 'errored_tokens:' + token_hash(token), "")
+      end
+    end
+
+    after do
+      tokens.each do |token|
+        redis.del(Travis::Addons::GithubStatus::Task::REDIS_PREFIX + 'errored_tokens:' + token_hash(token))
+      end
+    end
+
+    it "skips using the token" do
+      expect { run }.not_to raise_error
+      expect(io.string).to match /Token for svenfuchs failed/
+    end
   end
 
   describe 'logging' do
     it 'warns about a failed request' do
-      GH.stubs(:post).raises(GH::Error.new(nil))
+      GH.stubs(:post).raises(GH::Error.new(nil, nil, {response_headers: rate_limit_data}))
       expect {
         run
-      }.to raise_error
-      io.string.should include('error=not_updated')
-      io.string.should include('message=GH request failed')
+      }.to raise_error RuntimeError
+      expect(io.string).to include('error=not_updated')
+      expect(io.string).to include('message=GH request failed')
+      expect(io.string).to include('rate_limit=')
     end
 
     it "doesn't raise an error with bad credentials" do
-      error = { response_status: 401 }
+      error = { response_status: 401, response_headers: rate_limit_data }
       GH.stubs(:post).raises(GH::Error.new('failed', nil, error))
       expect {
         run
