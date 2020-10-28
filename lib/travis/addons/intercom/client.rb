9e7e0f43f6794434986f4e7a0cc25bf46df36fc8
Artur Smirnov <mail@artursmirnov.com>
2020-10-28 21:03:09 +0300

    Some more adjustmets

--- lib/travis/addons/intercom/client.rb
+++ lib/travis/addons/intercom/client.rb
@@ -12,20 +12,25 @@ module Travis
         end
 
         def report_build(params)
+          return unless @user
           @user.custom_attributes['fisrt_build_at'] = params[:fisrt_build_at] if params[:fisrt_build_at]
           @user.custom_attributes['last_build_at'] = params[:last_build_at]
           update_user
         end
 
         def report_subscription(params)
+          return unless @user
           @user.custom_attributes['has_subscription'] = params[:has_subscription]
+          update_user
         end
 
         private
 
         def get_user(id)
           @intercom.users.find(user_id: id)
-        rescue Intercom::ResourceNotFound
+        rescue ::Intercom::ResourceNotFound
+          @intercom.users.create(user_id: id) # Create an empty user to save events, name and email will be added by other sources, e.g. web
+        rescue
           nil
         end
 
