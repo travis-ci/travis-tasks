73344af2902abad8e79e059f2d639151a60a2eaa
Maciej Kempin <makemp@users.noreply.github.com>
2021-02-24 13:39:16 +0000

    Update user_confirmation.rb

--- lib/travis/addons/user_confirmation.rb
+++ lib/travis/addons/user_confirmation.rb
@@ -2,25 +2,22 @@
 
 require 'action_mailer'
 
-module Travis
-  module Addons
-    module UserConfirmation
-      module Mailer
+module Travis::Addons::UserConfirmation
+  module Mailer
+  end
+
+  require 'travis/addons/user_confirmation/task'
+
+  class << self
+    def setup
+      mailer = ActionMailer::Base
+      mailer.delivery_method = :smtp
+      mailer.smtp_settings = Travis.config.smtp
+        ActionMailer::Base.append_view_path("#{base_dir}/views")
       end
 
-      require 'travis/addons/user_confirmation/task'
-
-      class << self
-        def setup
-          mailer = ActionMailer::Base
-          mailer.delivery_method = :smtp
-          mailer.smtp_settings = Travis.config.smtp
-          ActionMailer::Base.append_view_path("#{base_dir}/views")
-        end
-
-        def base_dir
-          File.expand_path('trial/mailer', __dir__)
-        end
+      def base_dir
+        File.expand_path('trial/mailer', __dir__)
       end
     end
   end
