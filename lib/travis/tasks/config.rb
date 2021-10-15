ed2a6db1319381ef68f287b0c2f5b654284fbb3d
Stanislav Kolotinskiy <stanislav@assembla.com>
2021-10-15 10:19:33 +0300

    Remove accidental change

--- lib/travis/tasks/config.rb
+++ lib/travis/tasks/config.rb
@@ -29,7 +29,7 @@ module Travis
       define host:    "travis-ci.org",
              host_domain: 'travis-ci.com',
              github:  { url: 'https://github.com' },
-             redis:   { url: "redis://redis:6379" },
+             redis:   { url: "redis://localhost:6379" },
              sentry:  { },
              metrics: { reporter: 'librato' },
              sidekiq: { namespace: "sidekiq", pool_size: 3, retry: 4 },
