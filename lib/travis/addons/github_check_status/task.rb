74e12336201db8c554083b53d5a6d3fee5b2f663
piccadilly circus <baize.aspect0t@icloud.com>
2023-07-19 14:31:47 +0400

    send commit status to GitHub ship:docker

--- lib/travis/addons/github_check_status/task.rb
+++ lib/travis/addons/github_check_status/task.rb
@@ -69,6 +69,7 @@ module Travis
             puts "##############################"
             puts "##############################"
             puts "sending commit status to GitHub"
+            puts "The payload it #{{ process_via_gh_apps: false, id: repository[:vcs_id], type: repository[:vcs_type], ref: sha, payload: check_status_payload }}"
             create_commit_status = client.create_status(
               process_via_gh_apps: false,
               id: repository[:vcs_id],
