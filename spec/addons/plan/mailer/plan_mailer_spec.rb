089b3e0b9df245bfa969d34a4a4c2d47cfa8e760
GbArc <gabriel.arczynski@devtactics.net>
2024-02-19 13:13:01 +0100

    separate welcome msg for enterprise

--- spec/addons/plan/mailer/plan_mailer_spec.rb
+++ spec/addons/plan/mailer/plan_mailer_spec.rb
@@ -22,4 +22,17 @@ describe Travis::Addons::Plan::Mailer::PlanMailer do
       expect(mail.body).to match('Please select a plan in order to use Travis CI.')
     end
   end
+
+  describe '#welcome enterprise' do
+    before :each do
+      Travis.config.enterprise_platform.host = 'https://test-travis.com'
+      Travis.config.enterprise = true
+    end
+    subject(:mail) { described_class.welcome(recipients, owner, params) }
+
+    it 'contains the right data' do
+      expect(mail.to).to eq(recipients)
+      expect(mail.body).to match(Travis.config.enterprise_platform.host)
+    end
+  end
 end
