3469da712df7170158a4765cf97b9959c2e871ef
Dominik <dominik.alberski@gmail.com>
2024-10-23 13:07:21 +0200

    layout: remove debug logging

--- lib/travis/addons/slack/task.rb
+++ lib/travis/addons/slack/task.rb
@@ -23,14 +23,9 @@ module Travis
 
         def send_message(target, timeout)
           url, channel = parse(target)
-          begin
           response = http(base_url(url)).post(url) do |request|
             request.options.timeout = timeout
             request.body = MultiJson.encode(message(channel))
-          end
-          rescue Faraday::ConnectionFailed => e
-            warn "task=slack build=#{build[:id]} repo=#{repository[:slug]} error=connection_failed message=#{e.message}, response_status=#{response&.status} response_body=#{response&.body} response_headers=#{response&.headers}, error_backtrace=#{e.backtrace}, target=#{target}"
-            raise e
           end
           unless response.success?
             warn "task=slack build=#{build[:id]} repo=#{repository[:slug]} response_status=#{response.status} response_body=#{response.body}"
